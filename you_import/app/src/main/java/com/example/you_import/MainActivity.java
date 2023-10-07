d7d86c878405d9fdc3d4eac2227c4ff13357e0c4
Foxomax <119695708+Foxomax@users.noreply.github.com>
2023-10-06 21:56:26 -0600

    Add files via upload

---
+++ you_import/app/src/main/java/com/example/you_import/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.you_import;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void accionbtn(View view){
+        
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
